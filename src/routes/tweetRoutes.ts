0754ec4e204313d07efab6f35ba5b615e73a1cf4
MosesChisz <mosesc@cortexware.co.za>
2023-09-18 15:28:14 +0200

    first commit

---
+++ src/routes/tweetRoutes.ts
@@ -0,0 +1,33 @@
+import { Router } from "express";
+
+const router = Router();
+
+//create tweet
+router.post('/', (req, res) => {
+    res.status(501).json({ error: 'Not Implemented' });
+})
+
+//List tweets
+router.get('/', (req, res) => {
+    res.status(501).json({ error: 'Not Implemented' });
+})
+
+//get one tweet
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    res.status(501).json({ error: `Not Implemented: ${id} ` });
+})
+
+//update tweet
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    res.status(501).json({ error: `Not Implemented: ${id} ` });
+})
+
+//delete tweet
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    res.status(501).json({ error: `Not Implemented: ${id} ` });
+})
+
+export default router;
