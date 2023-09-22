d0568d505d75145f53427d8745ced32f20c96ccd
MosesChisz <mosesc@cortexware.co.za>
2023-09-22 15:50:15 +0200

    User

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -25,14 +25,18 @@ router.post('/', async (req, res) => {
 
 //List users
 router.get('/', async (req, res) => {
-    const allUsers = await prisma.user.findMany();
+    const allUsers = await prisma.user.findMany({/*select: {id: true, name: true, image: true}*/});
+    
     res.json(allUsers);
 })
 
 //get one user
 router.get('/:id', async (req, res) => {
     const { id } = req.params;
-    const user = await prisma.user.findUnique({where: {id: Number(id)}})
+    const user = await prisma.user.findUnique({
+        where: {id: Number(id)}, 
+        include: {tweets: true}
+    })
     res.json(user);
 })
 
